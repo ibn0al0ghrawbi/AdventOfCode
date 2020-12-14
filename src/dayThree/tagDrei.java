ed7e4d68e86d9bc112cc4754abe879926257f9cc
ibn0al0ghrawbi <75629323+ibn0al0ghrawbi@users.noreply.github.com>
2020-12-14 01:37:24 +0100

    2. Aufgabe Tag 3+input tag 4

--- src/dayThree/tagDrei.java
+++ src/dayThree/tagDrei.java
@@ -5,20 +5,44 @@ import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Scanner;
+import java.util.concurrent.TimeUnit;
 
 public class tagDrei {
 
 	public static ArrayList<String> liste;
 	public static char map[][];
 	
-	public static void main(String [] args) {
+	
+	public static void main(String [] args) throws InterruptedException {
 		
 		loadFile();
 		
 		convert();
+		//slipping(1,3);
+		System.out.println(rechne());
 		
-		slipping();
+	
+		
+	
 	}
+	
+	public static long rechne(){
+		long gesamt;
+		long a = slipping(1,1);
+		
+		
+		long b = slipping(1,3);
+		
+		long c = slipping(1,5);
+		
+		long d = slipping(1,7);
+	
+		long e = slipping(2,1);
+		
+		gesamt = a*b*c*d*e;
+		return gesamt;
+	}
+	
 	
 	public static void loadFile() { 		//writing the file line by line into a string arraylist, bcs it was easier that way for me
 		liste = new ArrayList<String>();
@@ -65,26 +89,28 @@ public class tagDrei {
 		}
 	}
 	
-	public static void slipping() {
-		
-		int x=1;
-		int y=3;
+	public static Integer slipping(int down,int right) {
+		convert();		//char array jedes mal neu initialisieren, da es sonst zu overflow fehlern in der berechnung mehrerer slips gibt
+		int x=down;
+		int y=right;
 		int countTrees = 0;
+		
 		
 		while(x<map.length) {
 			
 			if(map[x][y] == '#') {
-				countTrees++;
+				countTrees=countTrees+1;
 				map[x][y] = 'X'; //replace a hitted tree with an zero for graphical representation
 			}else if(map[x][y] == '.') {
 				map[x][y] = '0';
 			}
-			x=x+1;
-			y=(y+3)%31;
+			x=x+down;
+			y=(y+right)%31;
 			
 		}
-		printArray();
+		//printArray();
 		System.out.println("\nhitted trees: "+countTrees);
-		
+		return countTrees;	
+	
 	}
 }
